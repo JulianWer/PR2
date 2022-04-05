7d6361db66cab7d2a8d0ec2eae095a9db5ab11b5
Wolfgang Schramm <w.schramm@hs-mannheim.de>
2022-04-05 17:50:19 +0200

    1st Commit

---
+++ PR2-SoSe22-Stud/src/starterPackage/WelcomeToPR2.java
@@ -0,0 +1,13 @@
+package starterPackage;
+
+import static pr.MakeItSimple.*;
+
+public class WelcomeToPR2 {
+
+    public static void main(String[] args) {
+
+       println ("Willkommen in PR2!");
+       
+    }
+
+}
