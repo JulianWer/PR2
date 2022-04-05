71369971b36ea04b6a51e578d719d020730790a2
Wolfgang Schramm <w.schramm@hs-mannheim.de>
2022-04-05 18:08:53 +0200

    Init Übungen

---
+++ PR2-SoSe22-Stud/tests/uebung01/MergeSort1stTests.java
@@ -0,0 +1,14 @@
+package uebung01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MergeSort1stTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
