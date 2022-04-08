f69f8a8e21e943d051e9098d3f1429c5a0db7801
j.wernz <Baerle#2#5>
2022-04-08 13:01:28 +0200

    sort

--- PR2-SoSe22-Stud/src/AB1/direkterMergesort.java
+++ PR2-SoSe22-Stud/src/AB1/direkterMergesort.java
@@ -12,7 +12,7 @@ import static gdi.MakeItSimple.println;
 import static pr.MakeItSimple.*;
 
 public class direkterMergesort {
-
+	public static int durch = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,6 +20,7 @@ public class direkterMergesort {
 	}
 	
 	public static void sort(String path) {
+		durch++;
 		String tape_0 = path;
 		String tape_1 = "tape_1";
 		String tape_2 = "tape_2";
@@ -29,25 +30,41 @@ public class direkterMergesort {
 			return;
 		}
 
+		
 		Object tape0 = openInputFile(tape_0);
 		Object tape1 = openOutputFile(tape_1);
 		Object tape2 = openOutputFile(tape_2);
 		boolean tb = true;
-		
+		int l = 1;
 		while (!isEndOfInputFile(tape0)){
 			int in = readInt(tape_0);
 			if(tb)
 				print(tape1, in);
+				if(l == druch) {
+					
+				}
 			else
 				print(tape2,in);
 			
-			tb = !tb;
+			
 			
 		}
 		
 		closeInputFile(tape0);
 		closeOutputFile(tape1);
 		closeOutputFile(tape2);
+	}
+	
+	public static void merge(String path) {
+		String tape_0 = path;
+		String tape_1 = "tape_1";
+		String tape_2 = "tape_2";
+		Object tape0 = openOutputFile(tape_0);
+		Object tape1 = openInputFile(tape_1);
+		Object tape2 = openInputFile(tape_2);
+		
+		
+		
 	}
 	
 	
