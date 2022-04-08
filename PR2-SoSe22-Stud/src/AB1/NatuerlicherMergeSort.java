620e4e7d2a30948449fef08634e10c7f452c4f89
j.thiede <EP5dXdb3*p>
2022-04-08 12:40:02 +0200

    Testfaelle fuer natuerlichen MergeSort

--- PR2-SoSe22-Stud/src/AB1/NatuerlicherMergeSort.java
+++ PR2-SoSe22-Stud/src/AB1/NatuerlicherMergeSort.java
@@ -15,27 +15,26 @@ import static pr.MakeItSimple.*;
 public class NatuerlicherMergeSort {
 
 	public static void main(String[] args) {
-	
-		natuerlicherMergeSort("C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers02.txt", "C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\mergeMethodTest");
-		
-		
-		
+
+		natuerlicherMergeSort("C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers02.txt",
+				"C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\mergeMethodTest");
+
 	}
-	
+
 	public static void natuerlicherMergeSort(String srcFile, String newFile) {
-		//first run
+		// first run
 		split(srcFile);
 		merge();
 		do {
 			split(newFile);
 			merge();
-			
-		}while(checkIfFileHasMoreThanOneRun(newFile));
+
+		} while (checkIfFileHasMoreThanOneRun(newFile));
 	}
-	
+
 	public static void split(String path) {
-		//runs mit maximaler Lauflänge erstellen
-		//Tape0 aufteilen
+		// runs mit maximaler Lauflänge erstellen
+		// Tape0 aufteilen
 		String tape_0 = path;
 		String tape_1 = "tape_1";
 		String tape_2 = "tape_2";
@@ -44,58 +43,58 @@ public class NatuerlicherMergeSort {
 			println(tape_0 + " existiert nicht ");
 			return;
 		}
-		
+
 		Object tape0 = openInputFile(tape_0);
 		Object tape1 = openOutputFile(tape_1);
 		Object tape2 = openOutputFile(tape_2);
-		
+
 		boolean writeToTape1 = true;
 		int lastInputNumber = 0;
 		int currentNumber = 0;
 		int cnt = 0;
-		
-		while(!isEndOfInputFile(tape0)) {
-			if(cnt == 0) {
-				currentNumber = readInt(tape0);	//Initialisierung
+
+		while (!isEndOfInputFile(tape0)) {
+			if (cnt == 0) {
+				currentNumber = readInt(tape0); // Initialisierung
 				print(tape1, currentNumber + " ");
 				lastInputNumber = currentNumber;
 				cnt++;
 				continue;
 			}
-			
+
 			try {
 				currentNumber = readInt(tape0);
-			}catch (Exception e) {
+			} catch (Exception e) {
 				// TODO: handle exception
 				closeInputFile(tape0);
 				closeOutputFile(tape1);
 				closeOutputFile(tape2);
 				return;
 			}
-			
-			if(currentNumber < lastInputNumber)
+
+			if (currentNumber < lastInputNumber)
 				writeToTape1 = !writeToTape1;
-			
-			if(writeToTape1) {
+
+			if (writeToTape1) {
 				print(tape1, currentNumber + " ");
-			}else {
+			} else {
 				print(tape2, currentNumber + " ");
 			}
-			lastInputNumber	= currentNumber;
+			lastInputNumber = currentNumber;
 			cnt++;
-			
+
 		}
-		
+
 		closeInputFile(tape0);
 		closeOutputFile(tape1);
 		closeOutputFile(tape2);
 	}
-	
+
 	public static void merge() {
-		//tape0 überschreiben
-		//solange tape1 & tape2 einen Lauf enthalten
+		// tape0 überschreiben
+		// solange tape1 & tape2 einen Lauf enthalten
 		// => Füge das kleinere Anfangselement an tape0 hinzu
-		
+
 		String tape_0 = "C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers01.txt";
 		String tape_1 = "tape_1";
 		String tape_2 = "tape_2";
@@ -105,67 +104,80 @@ public class NatuerlicherMergeSort {
 			println(tape_0 + " existiert nicht ");
 			return;
 		}
-		
+
 		Object tape1 = openInputFile(tape_1);
 		Object tape2 = openInputFile(tape_2);
 		Object sortedTape = openOutputFile(sorted_tape);
-		
-		//Initialwerte
-		int currentElementTape1 = readInt(tape1);
-		int currentElementTape2 = readInt(tape2);
-		
+
 		boolean tape1ContainsRuns = true;
 		boolean tape2ContainsRuns = true;
-		
-		while(tape1ContainsRuns || tape2ContainsRuns) {	//solange die Runs noch nicht abgearbeitet sind
-			//append smaller element
-			if(((currentElementTape1 <= currentElementTape2) && tape1ContainsRuns) || (tape1ContainsRuns && !tape2ContainsRuns)) {
+
+		// Initialwerte
+		int currentElementTape1 = 0, currentElementTape2 = 0;
+		try {
+			currentElementTape1 = readInt(tape1);
+		} catch (Exception e) {
+			tape1ContainsRuns = false;
+		}
+		try {
+			currentElementTape2 = readInt(tape2);
+		} catch (Exception e) {
+			tape2ContainsRuns = false;
+		}
+
+		while (tape1ContainsRuns || tape2ContainsRuns) { // solange die Runs noch nicht abgearbeitet sind
+			// append smaller element
+			if (((currentElementTape1 <= currentElementTape2) && tape1ContainsRuns)
+					|| (tape1ContainsRuns && !tape2ContainsRuns)) {
 				print(sortedTape, currentElementTape1 + " ");
-				
-				
-				if(!isEndOfInputFile(tape1)) {
+
+				if (!isEndOfInputFile(tape1)) {
 					try {
 						currentElementTape1 = readInt(tape1);
-					}catch(Exception e) { tape1ContainsRuns = false; }
-					
-				}else {
+					} catch (Exception e) {
+						tape1ContainsRuns = false;
+					}
+
+				} else {
 					tape1ContainsRuns = false;
 				}
-				
-				
+
 			}
-			if((currentElementTape2 < currentElementTape1 && tape2ContainsRuns) || (!tape1ContainsRuns && tape2ContainsRuns)) {
-				//currentelement from tape2 is smaller
+			if ((currentElementTape2 < currentElementTape1 && tape2ContainsRuns)
+					|| (!tape1ContainsRuns && tape2ContainsRuns)) {
+				// currentelement from tape2 is smaller
 				print(sortedTape, currentElementTape2 + " ");
-				
-				if(!isEndOfInputFile(tape2)) {
+
+				if (!isEndOfInputFile(tape2)) {
 					try {
 						currentElementTape2 = readInt(tape2);
-					}catch(Exception e) {tape2ContainsRuns = false;}
-				}else {
+					} catch (Exception e) {
+						tape2ContainsRuns = false;
+					}
+				} else {
 					tape2ContainsRuns = false;
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		closeInputFile(tape1);
 		closeInputFile(tape2);
 		closeOutputFile(sortedTape);
 	}
-	
+
 	public static boolean checkIfFileHasMoreThanOneRun(String path) {
 		Object tape0 = openInputFile(path);
 		int lastElement = readInt(tape0);
-		while(!isEndOfInputFile(tape0)) {
+		while (!isEndOfInputFile(tape0)) {
 			try {
 				int currentElement = readInt(tape0);
-				if(lastElement > currentElement) {
+				if (lastElement > currentElement) {
 					closeInputFile(tape0);
 					return true;
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				closeInputFile(tape0);
 				return false;
 			}
--- PR2-SoSe22-Stud/tests/uebung01/MergeSort1stTests.java
+++ PR2-SoSe22-Stud/tests/uebung01/MergeSort1stTests.java
@@ -1,18 +1,59 @@
 package uebung01;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
+import static gdi.MakeItSimple.closeInputFile;
+import static gdi.MakeItSimple.closeOutputFile;
+import static gdi.MakeItSimple.openInputFile;
+import static gdi.MakeItSimple.openOutputFile;
+import static gdi.MakeItSimple.readInt;
+import static gdi.MakeItSimple.isEndOfInputFile;
+import static gdi.MakeItSimple.isFilePresent;
+import static gdi.MakeItSimple.print;
+import static gdi.MakeItSimple.println;
+import static pr.MakeItSimple.*;
 
 import org.junit.Test;
 
+import AB1.NatuerlicherMergeSort;
+
 public class MergeSort1stTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	/*
+	 * @Test public void test() { fail("Not yet implemented"); }
+	 */
 
 	@Test
 	public void natuerlicherMergeSortTest() {
+		//Testfaelle für die inputFiles
+		String path1 = "C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers01.txt";
+		String path2 = "C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers02.txt";
+		String path3 = "C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers03.txt";
+		String pathForTestFile = "C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\mergeMethodTest";
+		String paths[] = {path1, path2, path3};
+		
+		for (String path : paths) {
+			NatuerlicherMergeSort.natuerlicherMergeSort(path, pathForTestFile);
+			
+			Object file = openInputFile("C:\\Users\\Johannes\\git\\Team-03\\PR2-SoSe22-Stud\\mergeMethodTest");
+			int lastElement = readInt(file);
+			try {
+				while(!isEndOfInputFile(file)) {
+					int currentElement = readInt(file);
+					if(lastElement > currentElement)
+						fail("Falsche Reihenfolge => Algorithmus ist fehlerhaft");
+					lastElement = currentElement;
+				}
+			}catch (Exception e) {
+				assumeTrue("Algorithmus korrekt, alle Elemente in der richtigen Reihenfolge", true);
+			}
+			assumeTrue("Algorithmus korrekt, alle Elemente in der richtigen Reihenfolge", true);	//wird erst ausgeführt, wenn alle Element an der korrekten Position stehen
+			//checkIfCorrectSorted
+		}
+		
+		
+		
+		
 		
 	}
 }
