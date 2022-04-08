270abfc9eab5c959a83b7f087fb4eec66b855647
j.wernz <Baerle#2#5>
2022-04-08 12:51:47 +0200

    sort

--- PR2-SoSe22-Stud/src/AB1/direkterMergesort.java
+++ PR2-SoSe22-Stud/src/AB1/direkterMergesort.java
@@ -2,11 +2,14 @@ package AB1;
 
 import static gdi.MakeItSimple.closeInputFile;
 import static gdi.MakeItSimple.closeOutputFile;
-import static gdi.MakeItSimple.isEndOfInputFile;
-import static gdi.MakeItSimple.isFilePresent;
 import static gdi.MakeItSimple.openInputFile;
 import static gdi.MakeItSimple.openOutputFile;
+import static gdi.MakeItSimple.readInt;
+import static gdi.MakeItSimple.isEndOfInputFile;
+import static gdi.MakeItSimple.isFilePresent;
+import static gdi.MakeItSimple.print;
 import static gdi.MakeItSimple.println;
+import static pr.MakeItSimple.*;
 
 public class direkterMergesort {
 
@@ -29,8 +32,16 @@ public class direkterMergesort {
 		Object tape0 = openInputFile(tape_0);
 		Object tape1 = openOutputFile(tape_1);
 		Object tape2 = openOutputFile(tape_2);
+		boolean tb = true;
 		
 		while (!isEndOfInputFile(tape0)){
+			int in = readInt(tape_0);
+			if(tb)
+				print(tape1, in);
+			else
+				print(tape2,in);
+			
+			tb = !tb;
 			
 		}
 		
@@ -38,5 +49,7 @@ public class direkterMergesort {
 		closeOutputFile(tape1);
 		closeOutputFile(tape2);
 	}
+	
+	
 
 }
