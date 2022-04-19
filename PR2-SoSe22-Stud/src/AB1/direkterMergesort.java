bc88aa93e024bb981af8e83f0a744cffb96e44a1
j.wernz <Baerle#2#5>
2022-04-19 11:01:02 +0200

    merge sort fix completely functional

--- PR2-SoSe22-Stud/src/AB1/direkterMergesort.java
+++ PR2-SoSe22-Stud/src/AB1/direkterMergesort.java
@@ -10,260 +10,142 @@ import static gdi.MakeItSimple.print;
 import static gdi.MakeItSimple.println;
 import static gdi.MakeItSimple.readInt;
 
-import java.util.ArrayList;
-
-
-
 
 
 public class direkterMergesort {
-	public static int durchL = 0;
 
-	
-	
-	public  void direktMerge(String src ,String path) {
-		split(src);
-		merge(path);
-		do {
-			split(path);
-			merge(path);
-		}while(checkRun(path));
-		split(path);
-		merge(path);
-		split(path);
-		merge(path);
-	}
-	
-	public  boolean checkRun(String path) {
-		int array[] = new int[100];
-		int in =0;
-		boolean check = true;
-		Object tape0 = openInputFile(path);
-		while (!isEndOfInputFile(tape0)) {
-			try {
-				for(int i = 0 ; i<array.length ;i++)
-					array[i] = readInt(tape0);
-				
-			}catch(Exception e) {
-				closeInputFile(tape0);
-				println("fehler");
-				return false;
-			}
-			
-			for(int i :array) {
-				if (array[i]> array[i-1]) {
-					check = false;
-				}
-				else { 
-					return true;
-				}
-			}
-		}
-		closeInputFile(tape0);
-		return check;
+	public static void main(String[] args) {
+		runMergeSort("C:\\Users\\julia\\git\\Team-03\\PR2-SoSe22-Stud\\inputFiles\\numbers03.txt");
 	}
 
-	public  void split(String path) {
-		durchL++; // zählvariable für insgesamt durchläufe
-		String tape_0 = path;
+	public static void runMergeSort(String source) {
+
+		int cntElements = 1; //how much Elements are in the file (1 to activate the for loop)
+		// Files 
+		String tape_0 = source;
+		String tape0change = "C:\\Users\\julia\\git\\Team-03\\PR2-SoSe22-Stud\\mergeMethodTest";
 		String tape_1 = "tape_1";
 		String tape_2 = "tape_2";
-
-		if (!isFilePresent(tape_0)) {
-			println(tape_0 + " existiert nicht ");
-			return;
+		boolean check  = checkIfPresent(source,tape_1 , tape_2, tape0change); // check if the files are present
+		for (int runlength = 1; cntElements >= runlength && check; runlength *= 2) {
+			cntElements = split(tape_0,tape_1,tape_2, runlength);
+			tape_0 = tape0change;
+			merge(runlength,tape_0 );
+			
 		}
+	}
 
+	public static int split(String tape_0,String tape_1,String tape_2, int length) {
+
+		// initialize Object streams
 		Object tape0 = openInputFile(tape_0);
 		Object tape1 = openOutputFile(tape_1);
 		Object tape2 = openOutputFile(tape_2);
-		boolean tb = true;
-		if(durchL%2 != 0 && durchL !=1)
-			durchL+=1;
-		int d = durchL;
 		
-		
-		int l = 1;
-		int in = 0;
+		int cnt = 0; // count variable( how much elements are in the file)
+		int in = 0; // input 
 		while (!isEndOfInputFile(tape0)) {
-			try {
-				in = readInt(tape0);
-				
-			}catch(Exception e) {
-				closeInputFile(tape0);
-				closeOutputFile(tape1);
-				closeOutputFile(tape2);
-				println("fehler");
-				return;
-			}
-			if(l == d+1) {
-				l = 1;
-				tb = !tb;
-			}
-			if (tb) {
-				print(tape1, " " +in);
-				
 
-			} else {
-				print(tape2, " " +in);
-				
+			for (int i = 0; i < length; i++) { // loop to the length of the Math.pow(2,n)
+				if (!isEndOfInputFile(tape0)) { 
+					in = readInt(tape0); 
+					print(tape1, " " + in);
+					cnt++; // add one element to count number
+				}
 			}
-			l++;
-
+			for (int i = 0; i < length; i++) {
+				if (!isEndOfInputFile(tape0)) {
+					in = readInt(tape0);
+					print(tape2, " " + in);
+					cnt++;
+				}
+			}
 		}
-
+		//close the file streams
 		closeInputFile(tape0);
 		closeOutputFile(tape1);
 		closeOutputFile(tape2);
+		return cnt; //return number of Elements
 	}
 
-	public void merge(String path) {
+	public static void merge(int runlength, String path) {
+
 		String tape_0 = path;
 		String tape_1 = "tape_1";
 		String tape_2 = "tape_2";
-		if (!isFilePresent(tape_0)) {
-			println(tape_0 + " existiert nicht ");
-			return;
-		}
-		Object tape0 = openOutputFile(tape_0);
-		Object tape1 = openInputFile(tape_1);
-		Object tape2 = openInputFile(tape_2);
-		int dl = durchL;
-		int in1 = 0;
-		int in2 = 0;
-		int res = 0;
-		boolean lb = true;
 		
 		
-		
-
-		while (!isEndOfInputFile(tape1) || !isEndOfInputFile(tape2)) {
-			
-				for(int i = 0; i< dl*2;i++) {
-						in1 = readInt(tape1);
-						in2 = readInt(tape2);
-						
-						if(in1< in2 && in1<res) {
-							println(tape0,in1);
-							res = in2;
-						}
-						else if(in2 < in1 && in2<res) {
-							println(tape0,in2);
-							res = in1;
-						}
-				}
-					
-					
-						
-					
-					
-					
-				
-			
-			
-		
-			
-			
-		}
-		closeInputFile(tape1);
-		closeInputFile(tape2);
-		closeOutputFile(tape0);
-
-	}
-	
-	public void merge(int runlength, String path){
-		String tape_0 = path;
-		String tape_1 = "tape_1";
-		String tape_2 = "tape_2";
-		if (!isFilePresent(tape_0)) {
-			println(tape_0 + " existiert nicht ");
-			return;
-		}
 		Object target = openOutputFile(tape_0);
 		Object tape1 = openInputFile(tape_1);
 		Object tape2 = openInputFile(tape_2);
-		
-		//decide which run is smaller
-		int cntElementsReadTape1 = 0;
-		int cntElementsReadTape2 = 0;
-		int currentElementTape1 = 0;
+		int cntElementsWroteTape1 = 0;//counter for elements 
+		int cntElementsWroteTape2 = 0;
+		int currentElementTape1 = 0;// elements
 		int currentElementTape2 = 0;
-		boolean firstRun = true;
-		
-		
-		while(cntElementsReadTape1 != runlength && cntElementsReadTape2 != runlength){
-			if(firstRun) {
-				currentElementTape1 = readInt(tape1);
-				currentElementTape2 = readInt(tape1);
-				cntElementsReadTape1++;
-				cntElementsReadTape2++;
-				firstRun = false;
-				
-				if(currentElementTape1 <= currentElementTape2) {
-					print(target, currentElementTape1);
-					currentElementTape1 = readInt(tape1);
-					cntElementsReadTape1++;
-					continue;
-				}else {
-					print(target, currentElementTape2);
-					currentElementTape2 = readInt(tape2);
-					cntElementsReadTape2++;
-					continue;
-				}
-				
-			
-			}
-			
-		if((currentElementTape1 <= currentElementTape2 && cntElementsReadTape1 < runlength) || (cntElementsReadTape2 == runlength && cntElementsReadTape1 < runlength)) {
-			//print current element from tape1
-			print(target, " " + currentElementTape1);
-			currentElementTape1 = readInt(tape1);
-			cntElementsReadTape1++;
-			continue;
-		}
-		if((currentElementTape2 < currentElementTape1 && cntElementsReadTape2 < runlength) || (cntElementsReadTape1 == runlength && cntElementsReadTape2 < runlength)) {
-			//print current element from tape1
-			print(target, " " + currentElementTape2);
-			currentElementTape2 = readInt(tape2);
-			cntElementsReadTape2++;
-			continue;
-		}
-			
-		}
-		
-		
-		//compare both elements
-		
-		
-		//solange wiederholen bis beide Tapes keine Elemente mehr beeinhalten.
-		
-	}
+		boolean write1 = true; // boolean to check if written
+		boolean write2 = true;
 
-//	public void merge(int runlength){
-//		//decide which run is smaller
-//		
-//		while( ...bis beide Tapes keine Elemente mehr beeinhalten)
-//		currentElementTape1 = readInt(tape1);
-//		currentElementTape2 = readInt(tape2);
-//		
-//		if(currentElementTape1 <= currentElementTape2){
-//			//add run from tape1 to traget tape
-//			print(target, currentElementTape1);
-//			for(int i = 0; i < (runlength - 1); i++){	//(runlength - 1) because first element is already printed to the target	
-//				currentElementTape1 = readInt(tape1);
-//				print(target, " " + currentElementTape1);
-//			}
-//		}else{
-//			//add run from tape1 to traget tape
-//			print(target, currentElementTape2);
-//			for(int i = 0; i < (runlength - 1); i++){
-//				currentElementTape2 = readInt(tape2);
-//				print(target, " " + currentElementTape2);
-//			}
-//			
-//		}
-//		
-//		//solange wiederholen bis beide Tapes keine Elemente mehr beeinhalten.
-//		
-//	}
+		while (!isEndOfInputFile(tape1) || !isEndOfInputFile(tape2) || !write1 || !write2) {
+
+			if (!isEndOfInputFile(tape1) && cntElementsWroteTape1 < runlength && write1) {
+				// print current element from tape1
+				currentElementTape1 = readInt(tape1);
+				write1 = false; // set to false because the new element is not written 
+
+			}
+
+			if (!isEndOfInputFile(tape2) && cntElementsWroteTape2 < runlength && write2) {
+				// print current element from tape2
+				currentElementTape2 = readInt(tape2);
+
+				write2 = false;
+			}
+			// check if element 1 is smaller than element 2
+			if (!write1 && (currentElementTape1 <= currentElementTape2 || write2)) {
+				print(target, " " + currentElementTape1); //write element 1
+				write1 = true;// set true to activate the if block to read a new Int 
+				cntElementsWroteTape1++; // add one to counter 
+			// check if element 2 is smaller than element 1
+			} else if (!write2 && (currentElementTape2 <= currentElementTape1 || write1)) {
+				print(target, " " + currentElementTape2); // write element 2
+				write2 = true;
+				cntElementsWroteTape2++;
+			}
+			//set both counters to 0 when 
+			if (cntElementsWroteTape1 == runlength && cntElementsWroteTape2 == runlength) {
+				cntElementsWroteTape1 = 0;
+				cntElementsWroteTape2 = 0;
+			}
+		}// loop through all Elements
+
+		closeInputFile(tape2);
+		closeInputFile(tape1);
+		closeOutputFile(target);
+
+		
+
+	}
+	
+	public static boolean checkIfPresent(String path, String tape1, String tape2, String tape0) {
+		String tape_1 = tape1;
+		String tape_2 = tape2;
+		String tape_0 = tape0;
+		if (!isFilePresent(path)) {
+			println(path + " existiert nicht ");
+			return false;
+		}
+		if (!isFilePresent(tape_1)) {
+			println(tape_1 + " existiert nicht ");
+			return false;
+		}
+		if (!isFilePresent(tape_2)) {
+			println(tape_2 + " existiert nicht ");
+			return false;
+		}
+		if (!isFilePresent(tape_0)) {
+			println(tape_0 + " existiert nicht ");
+			return false;
+		}
+		return true;
+	}
 }
