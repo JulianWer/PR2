c01e7d3dbb37112891c8da2bc8d97271457f4001
j.wernz <Baerle#2#5>
2022-04-20 16:58:31 +0200

    change package

---
+++ PR2-SoSe22-Stud/src/uebung01/Start.java
@@ -0,0 +1,10 @@
+package uebung01;
+
+public class Start {
+
+	public static void main(String[] args) {
+		new UI().menu();
+
+	}
+
+}
---
+++ PR2-SoSe22-Stud/src/uebung01/UI.java
@@ -0,0 +1,31 @@
+package uebung01;
+import java.util.Scanner;
+
+import uebung01.NatuerlicherMergeSort;
+import uebung01.StraightMergeSort;
+
+public class UI {
+	
+	
+	public void menu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the path of the file: ");
+		String inputpath = sc.next();
+		System.out.println("Enter the path of the outputfile: ");
+		String outputpath = sc.next();
+		System.out.println("Select one of the following algorithms: ");
+		System.out.println("s - straigth");
+		System.out.println("n - natural");
+		char mode = sc.next().charAt(0);
+		switch(mode) {
+		case('s'):
+			new StraightMergeSort().sort(inputpath, outputpath);
+			break;
+		case('n'):
+			NatuerlicherMergeSort.natuerlicherMergeSort(inputpath, outputpath);
+			break;
+		default:
+			System.out.println("Fehlerhafte eingabe.");
+		}
+	}
+}
