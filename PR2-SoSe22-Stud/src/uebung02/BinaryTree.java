64932abdcfccb47dc4e90d484dba1cd6426628e1
j.thiede <EP5dXdb3*p>
2022-04-27 14:38:09 +0200

    Tree Class erstellt

---
+++ PR2-SoSe22-Stud/src/uebung02/BinaryTree.java
@@ -0,0 +1,127 @@
+package uebung02;
+
+import javax.lang.model.element.Element;
+
+public class BinaryTree implements Tree {
+
+	@Override
+	public boolean insert(Element val) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean insertFromFile(String filename) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveToFile(String filename) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Element val) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Element getMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Element getMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Element val) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Tree addAll(Tree otherTree) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printInorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printPostorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printPreorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printLevelorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Tree clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equal(Tree otherTree) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Tree createTree() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void visualize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
