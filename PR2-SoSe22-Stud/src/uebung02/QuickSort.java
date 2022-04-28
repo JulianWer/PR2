5c4a5af916fdffeb34318653d387c1acc3b90839
j.wernz <Baerle#2#5>
2022-04-28 16:32:56 +0200

    comments on qicksort v1

--- PR2-SoSe22-Stud/src/uebung02/QuickSort.java
+++ PR2-SoSe22-Stud/src/uebung02/QuickSort.java
@@ -1,78 +1,84 @@
 package uebung02;
+import static gdi.MakeItSimple.*;
 
-public class QuickSort implements SortInterface{
-	StatObject so;
+/**
+ * @author Johannes , Julian
+ * @since 2022-04
+ */
+
+public class QuickSort implements SortInterface {
+	StatObject so; // statistics collection
+
 	@Override
-	public void sort(int[] F, StatObject so) {
+	public void sort(int[] F, StatObject so) { // overloaded sort method for testing with parameters of an array and StatObject
 		// TODO Auto-generated method stub
-		this.so = so;
-		sort(F,0, F.length-1);
-		
+		this.so = so; // set StatObject to the attribute of itself
+		sort(F, 0, F.length - 1); // call the normal sort method
+
 	}
+
 	@Override
 	public void sort(int[] array, int lowerLimit, int upperLimit) {
 		System.out.println(upperLimit);
 		System.out.println(lowerLimit);
-		
-		if(upperLimit > lowerLimit) {
-			so.incrunCounter();
-			System.out.println("upperlimit is higher than lowerlimit");
-			int splitIndex = split(array, lowerLimit, upperLimit);
-			sort(array, lowerLimit, splitIndex);
-			sort(array, (splitIndex+1), upperLimit);
+
+		if (upperLimit > lowerLimit) { // runs as long as the upper cabinets is greater than the under
+			so.incrunCounter();// count runs for statistics
+			println("upperlimit is higher than lowerlimit");
+			int splitIndex = split(array, lowerLimit, upperLimit); // call split method and write the index to splitIndex
+			sort(array, lowerLimit, splitIndex); // call method recursive 
+			sort(array, (splitIndex + 1), upperLimit);
 		}
 	}
+
 	@Override
-	public int split(int[] array, int lowerLimit, int upperLimit) {	// Ordnet F bzgl. des mittleren Elements
+	public int split(int[] array, int lowerLimit, int upperLimit) { // sorts elements in direction of the middle element
 		// TODO Auto-generated method stub
-		int pivotIndex = (lowerLimit + upperLimit) / 2;
-		int pivot = array[pivotIndex];
-		System.out.println("Pivot: " + pivot);
-		
+		int pivotIndex = (lowerLimit + upperLimit) / 2; // set pivotIndex to the middle
+		int pivot = array[pivotIndex]; // set middle element of the array to the pivot element 
+		println("Pivot: " + pivot);
+
 		int l = upperLimit, r = lowerLimit;
-		while(lowerLimit <= upperLimit) {
-			
-			boolean found = false;
-			for(int i = lowerLimit; (i < (upperLimit + 1) && !found); i++) {
-				so.inccomparisonCounter();
-				if(array[i] >= pivot) {
-					found = true;
-					l = i;
+		while (lowerLimit <= upperLimit) { // run as long as upper is greater than lower
+
+			boolean found = false; // set boolean found to false
+			// for loop loops as long as i is smaller than upperLimit +1 and the boolean found is false
+			for (int i = lowerLimit; (i < (upperLimit + 1) && !found); i++) { 
+				so.inccomparisonCounter(); // count comparison Counter one up
+				if (array[i] >= pivot) { // compare values of index i and pivot element  
+					found = true; 
+					l = i; // set l to the value of i 
 				}
 			}
 			found = false;
-			for(int i = upperLimit; (i > (lowerLimit - 1) && !found); i--) {
+			// for loop loops as long as i is smaller than lowerLimit +1 and the boolean found is false
+			for (int i = upperLimit; (i > (lowerLimit - 1) && !found); i--) {
 				so.inccomparisonCounter();
-				if(array[i] <= pivot) {
+				if (array[i] <= pivot) {// compare values of index i and pivot element  
 					found = true;
 					r = i;
 				}
 			}
-			
-			if(l < r) {
-				int help = array[r];
-				array[r] = array[l];
-				array[l] = help;
-				so.incswapCounter();
-				lowerLimit = l + 1;
+
+			if (l < r) {
+				swap(array, r, l); // call swap method with array, r and l
+				so.incswapCounter(); // count swaps 
+				lowerLimit = l + 1; // add one to l and write it in lowerLimit
 				upperLimit = r - 1;
-			}else {
+			} else {
 				return r;
 			}
 		}
-		
-		
+
 		return upperLimit;
 	}
 
-	
-
-	
-
 	@Override
-	public void swap(int[] F, int i1, int i2) {
+	public void swap(int[] array, int r, int l) { // swap elements of array
 		// TODO Auto-generated method stub
-		
+		int temp = array[r]; // create temporary variable "temp" 
+		array[r] = array[l];
+		array[l] = temp;
 	}
 
 }
