1e6abbb7657f758f4779e99c0515a7fe98d0b0c5
j.wernz <Baerle#2#5>
2022-04-28 19:10:41 +0200

    add new tree interface and element class

---
+++ PR2-SoSe22-Stud/src/uebung02/Element.java
@@ -0,0 +1,8 @@
+package uebung02;
+
+public interface Element extends Comparable{
+	
+	public Object clone();
+	public Object getKey();
+
+}
---
+++ PR2-SoSe22-Stud/src/uebung02/IntElement.java
@@ -0,0 +1,23 @@
+package uebung02;
+
+public class IntElement implements Element, Cloneable {
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+    public Element clone() { // clone method from interface
+		// TODO Auto-generated method stub
+		return null;	
+	}
+
+}
