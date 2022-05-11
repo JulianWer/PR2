fd3041bde4bd5d138a98ed2bd6c86141ddba0ac7
j.wernz <Baerle#2#5>
2022-05-11 17:07:01 +0200

    tests

--- PR2-SoSe22-Stud/tests/uebung02/QuickSortV11stTest.java
+++ PR2-SoSe22-Stud/tests/uebung02/QuickSortV11stTest.java
@@ -1,56 +1,59 @@
 package uebung02;
 
-import pr2.uebung01.StatObjectSort;
+
+import uebung02.QickSort.QuickSort;
+import uebung02.QickSort.SortInterface;
 import uebung02.QickSort.StatObject;
-import pr2.uebung01.SortInterface;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import java.lang.invoke.MethodHandles;
 
 public class QuickSortV11stTest {
 
-	int[] F1;
-	int[] F2;
-	int[] F3;
+    int[] F1;
+    int[] F2;
+    int[] F3;
 
 
-	StatObject so;
+    StatObject so;
 
-	@Before
-	public void prepareTest() {
-		F1 = new int[] { 10, 4, 33, 44, 17, 20, 3, 2, 9, 82, 38, 67, 55, 11, 32, 23, 19, 7, 6, 14, 29 }; // zufälliges
-																											// Feld
-		F2 = new int[] { 10, 4, 33, 44, 17, 20, 3, 2, 9, 82, 38, 67, 55, 11, 32, 23, 19, 7, 6, 14, 29, 10, 10 }; // zufälliges
-																													// Feld
-																													// mit
-																													// Duplikaten
-		F3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // F ist schon sortiert;
+    @Before
+    public void prepareTest() {
+        F1 = new int[]{10, 4, 33, 44, 17, 20, 3, 2, 9, 82, 38, 67, 55, 11, 32, 23, 19, 7, 6, 14, 29}; // zufälliges
+        // Feld
+        F2 = new int[]{10, 4, 33, 44, 17, 20, 3, 2, 9, 82, 38, 67, 55, 11, 32, 23, 19, 7, 6, 14, 29, 10, 10}; // zufälliges
+        // Feld
+        // mit
+        // Duplikaten
+        F3 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // F ist schon sortiert;
 
-	}
+    }
 
-	@Test
-	public void testQuick11() { // QuickSort Variante 1
-		SortInterface sm = new QuickSortV1();
-		
-		so = new StatObject();
-		sm.sort(F1, so);
-		assertThat(F1, is(new int[] { 2, 3, 4, 6, 7, 9, 10, 11, 14, 17, 19, 20, 23, 29, 32, 33, 38, 44, 55, 67, 82 }));
+    @Test
+    public void testQuick11() { // QuickSort Variante 1
+        SortInterface sm = new QuickSort();
+
+        so = new StatObject();
+        sm.sort(F1, so);
+        assertThat(F1, is(new int[]{2, 3, 4, 6, 7, 9, 10, 11, 14, 17, 19, 20, 23, 29, 32, 33, 38, 44, 55, 67, 82}));
 //		assertEquals(122, so.getcomparisonCounter());
-		
-		so = new StatObject();
-		sm.sort(F2, so);
-		assertThat(F2,
-				is(new int[] { 2, 3, 4, 6, 7, 9, 10, 10, 10, 11, 14, 17, 19, 20, 23, 29, 32, 33, 38, 44, 55, 67, 82 }));
-//		assertEquals(142, so.getcomparisonCounter());
-		
-		so = new StatObject();
-		sm.sort(F3, so);
-		assertThat(F3, is(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
-//		assertEquals(43, so.getcomparisonCounter());
-		
 
-	}
+        so = new StatObject();
+        sm.sort(F2, so);
+        assertThat(F2,
+                is(new int[]{2, 3, 4, 6, 7, 9, 10, 10, 10, 11, 14, 17, 19, 20, 23, 29, 32, 33, 38, 44, 55, 67, 82}));
+//		assertEquals(142, so.getcomparisonCounter());
+
+        so = new StatObject();
+        sm.sort(F3, so);
+        assertThat(F3, is(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+//		assertEquals(43, so.getcomparisonCounter());
+
+
+    }
 
 }
