c2c2e49cdf41ba438c4d1059fea31287f7821d94
j.wernz <Baerle#2#5>
2022-05-20 11:35:20 +0200

    UI

--- PR2-SoSe22-Stud/src/uebung03/UI.java
+++ PR2-SoSe22-Stud/src/uebung03/UI.java
@@ -7,7 +7,7 @@ import static gdi.MakeItSimple.*;
 
 public class UI {
 
-    IntElement[] intArray = new IntElement[1];
+    Comparable[] intArray = new Comparable[1];
 
     public void menu(){
 
@@ -20,7 +20,7 @@ public class UI {
         String check;
         int indexcnt = 0;
         do{
-
+            dynamic(indexcnt);
             if(element.equals("str")){
 
             }else if(element.equals("int")){
@@ -36,7 +36,7 @@ public class UI {
 
     public void dynamic(int index){
         if (index >= intArray.length){
-            IntElement[] newArray = new IntElement[index+1];
+            Comparable[] newArray = new Comparable[index+1];
             for (int i = 0; i < intArray.length; i++) {
                 newArray[i] = intArray[i];
             }
