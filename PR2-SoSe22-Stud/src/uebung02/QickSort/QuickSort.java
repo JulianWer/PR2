78790c74ec3bb8125fe4046d0e61b5c8aa49a1d6
j.thiede <EP5dXdb3*p>
2022-05-20 10:13:40 +0200

    Comparable Interface QuickSort

--- PR2-SoSe22-Stud/src/uebung02/QickSort/QuickSort.java
+++ PR2-SoSe22-Stud/src/uebung02/QickSort/QuickSort.java
@@ -1,4 +1,6 @@
 package uebung02.QickSort;
+import uebung02.BinaryTree.IntElement;
+
 import static gdi.MakeItSimple.*;
 
 /**
@@ -6,10 +8,17 @@ import static gdi.MakeItSimple.*;
  * @since 2022-04
  */
 
-public class QuickSort implements SortInterface {
+public class QuickSort  {
 	StatObject so; // statistics collection
 
-	@Override
+	public static void main(String args[]){
+		IntElement array[] = {new IntElement(10), new IntElement(7), new IntElement(1), new IntElement(97), new IntElement(14), new IntElement(2), new IntElement(1),new IntElement(100) };
+		new QuickSort().sort(array, new StatObject());
+		for(Comparable t : array)
+			print(" " + ((IntElement) t).getValue());
+	}
+
+
 	public void sort(Comparable[] F, StatObject so) { // overloaded sort method for testing with parameters of an array and StatObject
 		// TODO Auto-generated method stub
 		this.so = so; // set StatObject to the attribute of itself
@@ -17,7 +26,7 @@ public class QuickSort implements SortInterface {
 
 	}
 
-	@Override
+
 	public void sort(Comparable[] array, int lowerLimit, int upperLimit) {
 		
 		if(array.length <= 1) so.incrunCounter();
@@ -31,11 +40,11 @@ public class QuickSort implements SortInterface {
 		}
 	}
 
-	@Override
+
 	public int split(Comparable[] array, int lowerLimit, int upperLimit) { // sorts elements in direction of the middle element
 		// TODO Auto-generated method stub
 		int pivotIndex = (lowerLimit + upperLimit) / 2; // set pivotIndex to the middle
-		int pivot = array[pivotIndex]; // set middle element of the array to the pivot element 
+		Comparable pivot = array[pivotIndex]; // set middle element of the array to the pivot element
 		
 
 		
@@ -45,16 +54,20 @@ public class QuickSort implements SortInterface {
 			// for loop loops as long as i is smaller than upperLimit +1 and the boolean found is false
 			int l = lowerLimit;
 			so.inccomparisonCounter();
-			while(array[l] < pivot) {
+			//while(array[l] < pivot) {
+			while(array[l].compareTo(pivot) < 0){
 				l++;
-				if (array[l] <= pivot)
+				//if (array[l] <= pivot)
+			if (array[l].compareTo(pivot) == 0 || array[l].compareTo(pivot) < 0)
 					so.inccomparisonCounter();
 			}
 			int r = upperLimit;
 			so.inccomparisonCounter();
-			while(array[r] > pivot) {
+			//while(array[r] > pivot) {
+			while(array[r].compareTo(pivot) > 0) {
 				r--;
-				if(array[r] >= pivot)
+				//if(array[r] >= pivot)
+				if(array[r].compareTo(pivot) > 0 || array[r].compareTo(pivot) == 0)
 					so.inccomparisonCounter(); // TODO abweichung um 1
 			}
 			
@@ -69,7 +82,7 @@ public class QuickSort implements SortInterface {
 			}
 		}
 		
-		for (int i : array) { // output after every run
+		for (Comparable i : array) { // output after every run
 			print(i + " ");
 		}
 		println();
@@ -77,10 +90,10 @@ public class QuickSort implements SortInterface {
 		return upperLimit;
 	}
 
-	@Override
-	public void swap(int[] array, int r, int l) { // swap elements of array
+
+	public void swap(Comparable[] array, int r, int l) { // swap elements of array
 		// TODO Auto-generated method stub
-		int temp = array[r]; // create temporary variable "temp" 
+		Comparable temp = array[r]; // create temporary variable "temp"
 		array[r] = array[l];
 		array[l] = temp;
 	}
