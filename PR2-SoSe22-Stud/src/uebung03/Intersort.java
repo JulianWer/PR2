d28dece6155e7843a719404d73f120e4cd33ae9f
j.thiede <EP5dXdb3*p>
2022-05-20 10:55:15 +0200

    Insertionsort mit Interface

--- PR2-SoSe22-Stud/src/uebung03/Intersort.java
+++ PR2-SoSe22-Stud/src/uebung03/Intersort.java
@@ -1,16 +1,47 @@
 package uebung03;
 
-public class Intersort {
-    public void insertionSort (Comparable [] array) {
+import uebung02.BinaryTree.IntElement;
+import uebung02.QickSort.StatObject;
 
+import static gdi.MakeItSimple.print;
+import static pr.MakeItSimple.println;
+
+public class Intersort implements SortInterface {
+
+
+    public static void main(String args[]){
+        println("InsertionSort");
+        uebung02.BinaryTree.IntElement array[] = {new uebung02.BinaryTree.IntElement(10), new uebung02.BinaryTree.IntElement(7), new uebung02.BinaryTree.IntElement(1), new uebung02.BinaryTree.IntElement(97), new uebung02.BinaryTree.IntElement(14), new uebung02.BinaryTree.IntElement(2), new uebung02.BinaryTree.IntElement(1),new uebung02.BinaryTree.IntElement(100) };
+        SortInterface.sortArray(array, new Intersort());
+        print("\n");
+        for(Comparable t : array)
+            print(" " + ((IntElement) t).getValue());
+    }
+    @Override
+    public void sort(Comparable[] array, int u, int o) {
         for (int i = 1; i < array.length; i++) {
             int j = i;
             Comparable m = array[i];
-            while (j > 0 && array[j - 1].compareTo(m) > 0 ) {
+            while (j > 0 && array[j - 1].compareTo(m) < 0 ) {
                 array[j] = array[j - 1];
                 j--;
             }
             array[j] = m;
         }
+    }
+
+    @Override
+    public void sort(Comparable[] F, StatObject so) {
+        this.sort(F, 0, F.length - 1);
+    }
+
+    @Override
+    public int split(Comparable[] F, int u, int o) {
+        return 0;
+    }
+
+    @Override
+    public void swap(Comparable[] F, int i1, int i2) {
+
     }
 }
