e84e0639d95431f72aba847483de2c3f78f3b665
j.thiede <EP5dXdb3*p>
2022-05-31 16:53:49 +0200

    put fix

--- PR2-SoSe22-Stud/src/uebung03/HashTable/HashTable.java
+++ PR2-SoSe22-Stud/src/uebung03/HashTable/HashTable.java
@@ -82,6 +82,8 @@ public class HashTable {
     }
 
     public Object put(Object key, Object value) {
+        if(this.size() / this.values.length >= 0.75)
+            this.reHash();
         int index = hashFunction(key);
         this.probing.startProbing();
         for (; ; ) {
